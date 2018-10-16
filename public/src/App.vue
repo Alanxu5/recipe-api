f446fdf715974f52658481327460991cd181a09d
Alan Xu <ayx002@ucsd.edu>
2018-10-16 16:11:08 -0700

    deletes skeleton code, adds vue store, creates simple recipe input page,

--- public/src/App.vue
+++ public/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Submit/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Submit from './components/Submit.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Submit
   }
 }
 </script>
---
+++ public/src/components/Submit.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>Submit your Recipe!</h1>
+    <label for="name">Enter recipe name</label>
+    <input id="name" size="30">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Submit',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
