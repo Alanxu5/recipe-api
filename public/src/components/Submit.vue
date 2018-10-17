04063d0a6d730a366f128fc5ae110c822019d44a
Alan Xu <ayx002@ucsd.edu>
2018-10-16 17:17:19 -0700

    adds the capability of submitting recipe data and saving it to the vuex store

--- public/src/components/Submit.vue
+++ public/src/components/Submit.vue
@@ -1,16 +1,53 @@
 <template>
-  <div class="hello">
+  <div>
     <h1>Submit your Recipe!</h1>
-    <label for="name">Enter recipe name</label>
-    <input id="name" size="30">
+    <form>
+      <div>
+        <label for="name">Enter recipe name</label>
+        <input id="name" v-model="name">
+      </div>
+      <div>
+        <label for="description">Enter description</label>
+        <input id="description" v-model="description">
+      </div>
+      <div>
+        <label for="ingredients">Enter ingredients</label>
+        <input id="ingredients" v-model="ingredients">
+      </div>
+      <div>
+        <label for="directions">Enter directions</label>
+        <input id="directions" v-model="directions">
+      </div>
+      <button @click="recipeData">Submit</button>
+    </form>
   </div>
 </template>
 
 <script>
+import { mapState, mapMutations } from 'vuex'
 export default {
   name: 'Submit',
-  props: {
-  }
+  data: function () {
+    return {
+      name: "",
+      description: "",
+      ingredients: "",
+      directions: "",
+    }
+  },
+  methods: {
+    ...mapMutations(['submitRecipe']),
+    recipeData(e) {
+      const recipe = {
+        name: this.name,
+        description: this.description,
+        ingredients: this.ingredients,
+        directions: this.directions,
+      };
+
+      this.submitRecipe(recipe);
+    }
+  },
 }
 </script>
 
--- public/src/store.js
+++ public/src/store.js
@@ -5,10 +5,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    recipe: {}
   },
   mutations: {
-
+    submitRecipe(state, recipe) {
+      state.recipe = recipe;
+    }
   },
   actions: {
 
