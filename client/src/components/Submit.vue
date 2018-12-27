b8f67686ef3caba64b5a2e44616609cc5fef05c7
Alan Xu <ayx002@ucsd.edu>
2018-12-26 21:26:34 -0800

    adds extra input fields for recipe submission

--- client/src/components/Submit.vue
+++ client/src/components/Submit.vue
@@ -2,48 +2,81 @@
   <div>
     <h1>Submit your Recipe!</h1>
     <form>
-      <div>
-        <label for="name">
-          Enter recipe name
-        </label>
-        <input
-          id="name"
-          v-model="name"
-        >
+      <div class="container">
+        <div class="left-side">
+          <div>
+            <label for="prepTime">
+              Enter prep time
+            </label>
+            <input
+              id="prepTime"
+              v-model="prepTime"
+            >
+          </div>
+          <div>
+            <label for="cookTime">
+              Enter cook time
+            </label>
+            <input
+              id="cookTime"
+              v-model="cookTime"
+            >
+          </div>         
+          <div>
+            <label for="feeds">
+              Enter number of people recipe feeds
+            </label>
+            <input
+              id="feeds"
+              v-model="feeds"
+            >
+          </div>                   
+        </div>
+        <div class="right-side">
+          <div>
+            <label for="name">
+              Enter recipe name
+            </label>
+            <input
+              id="name"
+              v-model="name"
+            >
+          </div>
+          <div>
+            <label for="description">
+              Enter description
+            </label>
+            <input
+              id="description"
+              v-model="description"
+            >
+          </div>
+          <div>
+            <label for="ingredients">
+              Enter ingredients
+            </label>
+            <input
+              id="ingredients"
+              v-model="ingredients"
+            >
+          </div>
+          <div>
+            <label for="directions">
+              Enter directions
+            </label>
+            <input
+              id="directions"
+              v-model="directions"
+            >
+          </div>
+          <button
+            type="button"
+            @click="recipeData"
+          >
+            Submit
+          </button>
+        </div>
       </div>
-      <div>
-        <label for="description">
-          Enter description
-        </label>
-        <input
-          id="description"
-          v-model="description"
-        >
-      </div>
-      <div>
-        <label for="ingredients">
-          Enter ingredients
-        </label>
-        <input
-          id="ingredients"
-          v-model="ingredients"
-        >
-      </div>
-      <div>
-        <label for="directions">
-          Enter directions
-        </label>
-        <input
-          id="directions"
-          v-model="directions"
-        >
-      </div>
-      <button
-        type="button"
-        @click="recipeData"
-      >
-        Submit
-      </button>
     </form>
   </div>
 </template>
@@ -58,6 +91,9 @@ export default {
       description: "",
       ingredients: "",
       directions: "",
+      prepTime: 0,
+      cookTime: 0,
+      feeds: 0,
     }
   },
   methods: {
@@ -68,6 +104,9 @@ export default {
         description: this.description,
         ingredients: this.ingredients,
         directions: this.directions,
+        prepTime: this.prepTime,
+        cookTime: this.cookTime,
+        feeds: this.feeds,
       };
 
       this.submitRecipe(recipe);
@@ -80,5 +119,18 @@ export default {
 <style scoped>
 h3 {
   margin: 40px 0 0;
+}
+
+.container {
+  display: grid;
+  grid-template-columns: 1fr 1fr
+}
+
+.left-side {
+  display: grid;
+}
+
+.right-side {
+  display: grid;
 }
 </style>
