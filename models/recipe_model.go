791ff5043fcb6d7cdbd85820b06071a40eaabe01
Alan Xu <ayx002@ucsd.edu>
2019-07-13 12:22:53 -0700

    able to create recipes

--- models/recipe_model.go
+++ models/recipe_model.go
@@ -2,17 +2,19 @@ package models
 
 import (
 	"database/sql"
+	"encoding/json"
 )
 
 type Recipe struct {
-	ID          int    `json:id`
-	Name        string `json:name`
-	Description string `json:description`
-	Ingredients string `json:ingredients`
-	Directions  string `json:directions`
-	PrepTime    int    `json:prep_time`
-	CookTime    int    `json:cook_time`
-	Feeds       int    `json:feeds`
+	Name        string          `json:"name"`
+	Description string          `json:"description"`
+	Ingredients string          `json:"ingredients"`
+	Directions  json.RawMessage `json:"directions"`
+	PrepTime    int             `json:"prepTime"`
+	CookTime    int             `json:"cookTime"`
+	Feeds       int             `json:"feeds"`
+	Type        int             `json:"type"`
+	Method      int             `json:"method"`
 }
 
 type RecipeCollection struct {
@@ -20,7 +22,7 @@ type RecipeCollection struct {
 }
 
 func GetRecipes(db *sql.DB) RecipeCollection {
-	sql := "SELECT * FROM recipes"
+	sql := "SELECT * FROM recipe"
 
 	rows, err := db.Query(sql)
 
@@ -34,7 +36,7 @@ func GetRecipes(db *sql.DB) RecipeCollection {
 
 	for rows.Next() {
 		recipe := Recipe{}
-		err := rows.Scan(&recipe.ID, &recipe.Name)
+		// err := rows.Scan(&recipe.ID, &recipe.Name)
 
 		if err != nil {
 			panic(err)
@@ -46,10 +48,20 @@ func GetRecipes(db *sql.DB) RecipeCollection {
 }
 
 func CreateRecipe(db *sql.DB, recipe Recipe) (int64, error) {
-	var lastInsertId int64
-	err := db.QueryRow("INSERT INTO recipes (name, description, prep_time, cook_time, feeds) VALUES ($1, $2, $3, $4, $5) RETURNING id", recipe.Name, recipe.Description, recipe.PrepTime, recipe.CookTime, recipe.Feeds).Scan(&lastInsertId)
-
+	jsonString, err := json.Marshal(recipe.Directions)
 	if err != nil {
+		panic(err)
+	}
+
+	var lastInsertId int64
+	res, err := db.Exec("INSERT INTO recipe (name, description, prep_time, cook_time, servings, method, type, directions) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", recipe.Name, recipe.Description, recipe.PrepTime, recipe.CookTime, recipe.Feeds, recipe.Method, recipe.Type, jsonString)
+	if err != nil {
+		panic(err)
+	}
+
+	lastInsertId, error := res.LastInsertId()
+
+	if error != nil {
 		panic(err)
 	}
 
